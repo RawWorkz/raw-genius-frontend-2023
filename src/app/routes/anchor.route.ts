d635ecc9416ce0f44f8ecd64faa9d63e8ec44f92
Matthijs Kamstra <matthijs@matthijskamstra.nl>
2023-06-21 09:53:31 +0200

    nl texten

--- src/app/routes/anchor.route.ts
+++ src/app/routes/anchor.route.ts
@@ -4,7 +4,7 @@ export type AnchorRoutes = {
 };
 
 export const anchorRoutes: AnchorRoutes[] = [
-	{ id: "about", title: "About" },
+	{ id: "about", title: "Over" },
 	// { id: "register", title: "Register" },
 	// { id: "speakers", title: "Speakers" },
 	{ id: "programma", title: "Programma" },
@@ -15,5 +15,5 @@ export const anchorRoutes: AnchorRoutes[] = [
 	// { id: "sponsors", title: "Sponsors" },
 	{ id: "zet-um-in-je-agenda", title: "Agenda" },
 	{ id: "eventvideo", title: "RawGenius 2022" },
-	{ id: "location", title: "Location" },
+	{ id: "location", title: "Locatie" },
 ];
